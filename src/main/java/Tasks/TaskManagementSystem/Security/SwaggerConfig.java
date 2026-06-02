30ae94474d7349c16579da3052f8e1956fad9058
Nagarajan2611 <mnagarajan967@gmail.com>
2026-06-03 03:58:50 +0530

    Added New

--- src/main/java/Tasks/TaskManagementSystem/Security/SwaggerConfig.java
+++ src/main/java/Tasks/TaskManagementSystem/Security/SwaggerConfig.java
@@ -12,23 +12,13 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
         final String securitySchemeName = "bearerAuth";
-
-        return new OpenAPI()
-                .addSecurityItem(
-                        new SecurityRequirement().addList(securitySchemeName)
-                )
-                .components(
-                        new Components()
-                                .addSecuritySchemes(
-                                        securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                )
-                );
+         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,new SecurityScheme()
+                                                                                    .name(securitySchemeName)
+                                                                                    .type(SecurityScheme.Type.HTTP)
+                                                                                    .scheme("bearer")
+                                                                                    .bearerFormat("JWT")));
     }
 }
